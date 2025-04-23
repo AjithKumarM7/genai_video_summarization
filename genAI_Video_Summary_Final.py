38e8a8881755588099ea32bbceafab58c34456e6
Ajith Kumar M <ajitharun48862@gmail.com>
2025-04-23 12:12:42 +0530

    Update genAI_Video_Summary_Final.py

--- genAI_Video_Summary_Final.py
+++ genAI_Video_Summary_Final.py
@@ -41,26 +41,26 @@ def extract_frames(video_path, interval_seconds=5):
   for file in os.listdir(frames_directory):
     os.remove(os.path.join(frames_directory, files))
 
- vidoes = cv2.VideoCapture(video_path)
- fps = int(video.get(cv2.CAP_PROP_FPS))
- frames_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+  vidoes = cv2.VideoCapture(video_path)
+  fps = int(video.get(cv2.CAP_PROP_FPS))
+  frames_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+ 
+  current_frame = 0
+  frame_number = 1
 
- current_frame = 0
- frame_number = 1
+  while current_frame <= frames_count:
+     video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
+     success, frame = video.read()
+     if not success:
+       current_frame += fps * interval_seconds
+       continue
 
- while current_frame <= frames_count:
-   video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-   success, frame = video.read()
-   if not success:
+     frame_path = os.path.join(frames_directory, f"frame_{frame_number:03d}.jpg")
+     cv2.imwrite(frame_path, frame)
      current_frame += fps * interval_seconds
-     continue
-
-  frame_path = os.path.join(frames_directory, f"frame_{frame_number:03d}.jpg")
-  cv2.imwrite(frame_path, frame)
-  current_frame += fps * interval_seconds
-  frame_number += 1
-
- video.release()
+     frame_number += 1
+   
+  video.release()
 
 def describe_video():
   descriptions = []
@@ -79,7 +79,7 @@ def turn_into_story(summary):
   return model.invoke(prompt)
 
 st.title("AjithAI - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("AjithAI_Transperent.png")
+st.image("AjithAI_Transperent.jpg")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
@@ -129,14 +129,3 @@ if "summary" in st.session_state:
             story = turn_into_story(st.session_state["summary"])
             st.markdown("### Cinematic Story:")
             st.markdown(story)
-
-
-
-
-
-
- 
-
-
-
-     
